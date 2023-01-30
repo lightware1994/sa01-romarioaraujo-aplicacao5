4db906ce657a31601cb8794486ce0b7ed041f0fe
lightware1994 <romarioaraujo12@gmail.com>
2023-01-30 17:27:51 -0300

    Desafio git java5

---
+++ src/br/com/senai/java/sa1aplicacao5/Principal.java
@@ -0,0 +1,29 @@
+package br.com.senai.java.sa1aplicacao5;
+
+import javax.swing.JOptionPane;
+
+import br.com.senai.java.sa1app04.PessoaBO;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,"Empresa Voe, Grupo Empresarial");
+		
+		PessoaBO tbo = new PessoaBO();
+		
+		String resp=JOptionPane.showInputDialog("Informe o primeiro mês:");
+		String resp1=JOptionPane.showInputDialog("Informe o segundo mês:");
+		String resp2=JOptionPane.showInputDialog("Informe o terceiro mês:");
+		
+		double valorMes1 = Double.parseDouble(resp);
+		double valorMes2 = Double.parseDouble(resp1);
+		double valorMes3 = Double.parseDouble(resp2);
+	
+		JOptionPane.showMessageDialog(null, "Resultado dos trimestre:" + String.format("%.2f", tbo.calcularMedia(valorMes1, valorMes2, valorMes3) ));
+		
+		
+
+	}
+
+}
