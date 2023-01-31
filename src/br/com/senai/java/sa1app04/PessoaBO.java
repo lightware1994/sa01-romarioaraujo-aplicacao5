61dbdd9f7a1382b7a483e8009b3aabf3a1a342e0
lightware1994 <romarioaraujo12@gmail.com>
2023-01-30 23:47:36 -0300

    Desafio git java5

--- src/br/com/senai/java/sa1app04/PessoaBO.java
+++ src/br/com/senai/java/sa1app04/PessoaBO.java
@@ -2,7 +2,7 @@ package br.com.senai.java.sa1app04;
 
 public class PessoaBO {
 
-	public double calcularMedia(double valor, double valor1,double valor2){
+	public double calcularMedia (double valor, double valor1,double valor2){
 		return (valor+valor1+valor2)/3;
 		
 	}
